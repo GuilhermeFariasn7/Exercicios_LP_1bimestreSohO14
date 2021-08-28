80bcdd855f5f65fc348c5b349e65efab0b21f6d5
Guilherme Farias Nazário <88680855+GuilhermeFariasn7@users.noreply.github.com>
2021-08-28 08:30:55 -0300

    Create Exer 14.py

---
+++ Exer 14.py
@@ -0,0 +1,12 @@
+n = int(input('\033[4;34mEscreva um número inteiro para saber sua tabuada:\033[m '))
+
+print(f'\033[0;31m{n} x 1 = {n} ')
+print(f'\033[0;31m{n} x 2 = {n*2} ')
+print(f'\033[0;31m{n} x 3 = {n*3} ')
+print(f'\033[0;31m{n} x 4 = {n*4} ')
+print(f'\033[0;33m{n} x 5 = {n*5} ')
+print(f'\033[0;33m{n} x 6 = {n*6} ')
+print(f'\033[0;32m{n} x 7 = {n*7} ')
+print(f'\033[0;32m{n} x 8 = {n*8}')
+print(f'\033[0;32m{n} x 9 = {n*9}')
+print(f'\033[0;32m{n} x 10 = {n*10}')
